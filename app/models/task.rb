dc34aaa51fa8747784196913667ec45dc2256c62
BrunaNett <beedeveloping@gmail.com>
2016-01-25 19:44:39 -0600

    add validations to task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,9 @@
 class Task < ActiveRecord::Base
 	belongs_to :user
 
+	validates :user_id, presence: true #everytime a task is created make sure there is a user_id associated with it
+	validates :content, presence: true #prevents user from adding empty task - tasks w/o content
+
 	auto_html_for :content do
 		html_escape
 		image
